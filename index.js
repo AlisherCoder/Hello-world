348ecf36038da80359cd0778b4991162b65d39df
Alisher Sharipov <alishersharipov670@gmail.com>
2025-02-06 20:53:24 +0500

    update' ;

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (req, res) => {
-   res.status(200).json({message: "Hello Coder"});
+   res.status(200).json({message: "Hello nimadir"});
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
