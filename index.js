1bcee3be65a6d45e1d3e70ebe0d1d56e646abefc
Alisher Sharipov <alishersharipov670@gmail.com>
2025-02-06 20:29:52 +0500

    dunyo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (req, res) => {
-   res.status(200).send({message: "Hello world"});
+   res.status(200).send({message: "Hello dunyo"});
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
